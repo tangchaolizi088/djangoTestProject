1443e97d603bca14d0d2d23ca3ed2fb57956c1bc
wangdx <348759948@qq.com>
2019-02-16 22:34:34 +0800

    支持留言板新增数据

--- apps/message/views.py
+++ apps/message/views.py
@@ -2,9 +2,23 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from models import UserMessage
 # Create your views here.
 
 
 def getform(request):
+    if request.method == 'POST':
+        name = request.POST.get('name','')
+        message = request.POST.get('message','')
+        address = request.POST.get('address','')
+        email = request.POST.get('email','')
+
+        user_message = UserMessage()
+        user_message.name = name
+        user_message.message = message
+        user_message.address = address
+        user_message.email = email
+        user_message.save()
+
+
     return render(request,'message_form.html')
